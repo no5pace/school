c7fdfe491eda062bbdf5f179e6fa023b79a157a4
Isaac <isaaclongo05@gmail.com>
2022-01-27 14:13:59 +0000

    edited the exception.py file

--- assignments/12_exception.py
+++ assignments/12_exception.py
@@ -5,18 +5,20 @@ Exception Handling
 1/26/22
 '''
 
-# error-proof function to find the perimeter of a rectangle
-
+# function to find the perimeter of a rectangle (no error proofing)
 def perimeter(x, y):
+	return (2 * (x + y))
+
+# error proofed here
+while (True):
 	try:
+		x = float(input("Length of the rectangle: "))
+		y = float(input("Height of the rectangle: "))
+		
 		if (x > 0 and y > 0):
-			return (2*x + 2*y)
+			print(perimeter(x, y))
+			break
 		else:
-			print("Invalid input... pick a positive number")
+			print("Invalid input... pick a positive number\n")
 	except:
-		print("Invalid input... pick a positive number")
-
-x = float(input("Length of the rectangle: "))
-y = float(input("Height of the rectangle: "))
-
-print(perimeter(x, y))
+		print("Invalid input... pick a positive number\n")
